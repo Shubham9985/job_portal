46fe1317b64ed37b4ca3163ef56f4f4172b265e7
Shubham <shubham9985kumar@gmail.com>
2025-12-22 20:23:28 +0530

    completed job list and job filter section with outline font

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import  {createContext} from 'react';
+import React, { useEffect } from "react";
+import  {createContext , useState} from 'react';
+import { jobsData } from "../assets/assets";
+
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
+
+    const [searchFilter , setSearchFilter ] = useState(
+        {
+            title : '',
+            location : ''
+        }
+    )
+
+    const [isSearched , setIsSeached] = useState(false)
+
+    const[jobs , setJobs ] = useState([])
+
+    //function to fetch jobs
+    const fetchJobs = async()=>{
+        setJobs(jobsData)
+    }
+
+    useEffect(()=>{
+        fetchJobs()
+    },[])
     
     const value = {
-
+        setSearchFilter , searchFilter ,
+        isSearched, setIsSeached , jobs , setJobs
     }
 
     return (<AppContext.Provider value={value}>
