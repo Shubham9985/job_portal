76832ab80c6f7dc9b1ba294b2f8994b9d3e05c9c
Shubham <shubham9985kumar@gmail.com>
2025-12-23 20:41:35 +0530

    Completed Footer and app download section along with phone dimensions

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,10 +1,14 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations, jobsData } from '../assets/assets'
 import JobCard from './JobCard'
 
 const JobListing = () => {
+
+    const [showFilter , setShowFilter] = useState(false)
     const {isSearched , searchFilter , setSearchFilter , jobs} = useContext(AppContext)
+    const [currentPage , setCurrentPage] = useState(1)
+
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
         {/*sidebar */}
@@ -32,8 +36,12 @@ const JobListing = () => {
                 )
             }
 
+            <button onClick={e => setShowFilter(prev => !prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+                {showFilter ? "Close" : "Filters" }
+            </button> {/*this effect is for moile view */}
+
             {/*category filter */}
-            <div className='max-lg:hidden'>
+            <div className={showFilter ? " " : "max-lg:hidden"}>
                 <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
                 <ul className='space-y-4 text-gray-600'>
                     {
@@ -48,7 +56,7 @@ const JobListing = () => {
             </div>
 
             {/*Location filter */}
-            <div className='max-lg:hidden'>
+            <div className={showFilter ? " " : "max-lg:hidden"}>
                 <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
                 <ul className='space-y-4 text-gray-600'>
                     {
@@ -69,10 +77,28 @@ const JobListing = () => {
         <h3 className='font-medium text-3xl py-2' id='job-list'>Lates Jobs</h3>
         <p className='mb-8'>Get your desired jobs from top Companies.</p>
         <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-            {jobs.map((job,index)=>(
+            {jobs.slice((currentPage-1)*6 , currentPage*6).map((job,index)=>(
                 < JobCard key={index} job={job}/>
             ))}
         </div>
+
+
+        {/*pagination   */}
+        {jobs.length > 0 && (
+            <div className='flex items-center justify-center space-x-2 mt-10 '>
+                <a href='#job-list'>
+                    <img onClick={() => setCurrentPage(Math.max(currentPage-1),1)} src={assets.left_arrow_icon}/>
+                </a>
+                {Array.from({length : Math.ceil(jobs.length/6)}).map((_,index)=>(
+                    <a href='#job-list'>
+                        <button onClick={()=> setCurrentPage(index + 1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage === index + 1 ? 'bg-blue-200 text-blue-500' : 'text-gray-500'}`}>{index +1}</button>
+                    </a>
+                ))}
+                <a href='#job-list'>
+                    <img onClick={() => setCurrentPage(Math.min(currentPage+1 , Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon}/>
+                </a>
+            </div>
+        )}
         </section>
         
 
